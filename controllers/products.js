b3a11d4809182e2501d00a76fc2c1f0d090874d8
Connor Mellencamp <comellen91@gmail.com>
2019-01-17 20:26:27 -0500

    adding views. adding navigation links. view folder restructure

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('admin/add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -12,7 +12,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('shop/products', {
             products: products,
             docTitle: 'Shop',
             path: '/'
