e50dbd0502f36e1796297da4d4fad8c653101ac2
Connor Mellencamp <comellen91@gmail.com>
2019-01-22 14:52:20 -0500

    cart model logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -40,6 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const id =  req.body.productId;
+    Product.findById(id, (product) => {
+        Cart.addProduct(id, product.price);
+    })
     res.redirect('/cart')
 };
 
